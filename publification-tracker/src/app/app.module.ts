b176b99e503bed3f78d579b91d88ec271dc0aa07
Adegat <adel.gatoui@gmail.com>
2017-02-06 14:12:01 +0200

    Created nav-bar, search-page and tracking-page with angular-cli

--- publification-tracker/src/app/app.module.ts
+++ publification-tracker/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { TrackingPageComponent } from './tracking-page/tracking-page.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    TrackingPageComponent,
+    SearchPageComponent
   ],
   imports: [
     BrowserModule,
---
+++ publification-tracker/src/app/tracking-page/tracking-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tracking-page',
+  templateUrl: './tracking-page.component.html',
+  styleUrls: ['./tracking-page.component.scss']
+})
+export class TrackingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
