4a0b0b13a71326c1110bf2e99b8477cd660efa30
Mew-www <mew.proxy@hotmail.com>
2017-02-17 00:28:15 +0200

    Report 9x adc int feed

--- publification-tracker/src/app/search-page/search-page.component.ts
+++ publification-tracker/src/app/search-page/search-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, HostListener} from '@angular/core';
 import {ThemoviedbService} from "../services/themoviedb.service";
 import {Publification} from "../interfaces/publification";
 import {Observable} from "rxjs";
@@ -35,7 +35,7 @@ export class SearchPageComponent implements OnInit {
     this.themoviedbService.tvshow_results$.subscribe(results => { this.results = this.results.concat(results); });
   }
 
-  onClickSearch = (search_query_string) => {
+  onStartSearch = (search_query_string) => {
     if (this.isServicesStandby() && !this.loading) {
 
       // Reset previous results, and mark we're starting to load
@@ -50,6 +50,47 @@ export class SearchPageComponent implements OnInit {
         this.searchEverInitialized = true;
       }
     }
+  };
+
+  private konamiqueue: string[] = [];
+  @HostListener('document:keyup', ['$event'])
+  onGlobalKeypress(event) {
+    const zettai_sequence = [
+      ['ArrowUp'], // #1
+      ['ArrowUp'], // #2
+      ['ArrowDown'], // #3
+      ['ArrowDown'], // #4
+      ['ArrowLeft'], // #5
+      ['ArrowRight'], // #6
+      ['ArrowLeft'], // #7
+      ['ArrowRight'], // #8
+      ['b', 'B'], // #9
+      ['a', 'A']  // #10!
+    ];
+    this.konamiqueue.push(event.key);
+    if (this.konamiqueue.length < zettai_sequence.length)
+      return;
+    if (this.konamiqueue.length > zettai_sequence.length)
+      this.konamiqueue.shift();
+    for (let i in this.konamiqueue) {
+      if (zettai_sequence[i].indexOf(this.konamiqueue[i]) === -1)
+        return;
+    }
+    let q = window.confirm(`
+         (\\_/)\u00A0\u00A0\u00A0 \u00A0 \u00A0(\\__/)\u00A0\u00A0\u00A0\u00A0 (\\_/)
+       =(^.^)=\u00A0\u00A0\u00A0 (>'.'<)\u00A0\u00A0\u00A0 (o.o)
+       (")_(")\u00A0\u00A0\u00A0 \u00A0(¨)_(¨)\u00A0\u00A0\u00A0 (¨)(¨)*
+       
+\u2610\u2610\u2610
+    `);
+    if (q)
+      alert(`
+        /|ˎ
+      (˚◟ₒ 7
+        | \u00A0 \u00A0~
+        |ˎ \u00A0 \u00A0 \u00A0ヽ
+        じしf_,)ノ
+      `);
   };
 
 }
