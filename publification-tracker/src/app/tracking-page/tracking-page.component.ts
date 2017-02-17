4890ecc8914d38b41ead8ad31187f8196dc91323
Adegat <adel.gatoui@gmail.com>
2017-02-17 15:10:27 +0200

    tracking page table and localstorage remove

--- publification-tracker/src/app/tracking-page/tracking-page.component.ts
+++ publification-tracker/src/app/tracking-page/tracking-page.component.ts
@@ -7,10 +7,25 @@ import {ThemoviedbService} from "../services/themoviedb.service";
   styleUrls: ['./tracking-page.component.scss']
 })
 export class TrackingPageComponent implements OnInit {
+  private movies;
 
   constructor(private themoviedbService: ThemoviedbService) { }
 
   ngOnInit() {
+    localStorage.setItem("tracked", JSON.stringify(
+      [{PDate:"19561",Name:"aedfed",Type:"movie",Genre:"Action, Comedy"},
+        {PDate:"5219",Name:"sdfrfvs",Type:"movie",Genre:"Drama, Comedy"}]
+    ));
+    this.movies=JSON.parse(localStorage.getItem("tracked"));
   }
+private Onclick =(PDate,Name)=>{
+  let Remove = JSON.parse(localStorage.getItem("tracked"));
+  for(var i = Remove.length-1; i>=0; i--){
+    console.log(PDate,Name);
+    if (Remove[i].PDate === PDate && Remove[i].Name === Name) Remove.splice(i, 1);
+    console.log(Remove);
+  }
+  localStorage.setItem("tracked" ,JSON.stringify(Remove));
 
+}
 }
