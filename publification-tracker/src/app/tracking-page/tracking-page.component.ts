d37b6880effbf332b88827b6afc66415a5ca49a4
Adegat <adel.gatoui@gmail.com>
2017-03-06 13:41:59 +0200

    in componenet trackin page table order by date and type filter

--- publification-tracker/src/app/tracking-page/tracking-page.component.ts
+++ publification-tracker/src/app/tracking-page/tracking-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ThemoviedbService} from "../services/themoviedb.service";
+import {Publification} from "../interfaces/publification";
+import {PublificationType} from "../enums/publification-type";
 
 @Component({
   selector: 'app-tracking-page',
@@ -7,25 +9,61 @@ import {ThemoviedbService} from "../services/themoviedb.service";
   styleUrls: ['./tracking-page.component.scss']
 })
 export class TrackingPageComponent implements OnInit {
-  private movies;
+  private movies:Array<Publification>;
+  private originalmovies:Array<Publification>;
+  private diplayedtypes:Array<PublificationType>=[];
+  private movietype=PublificationType.MOVIE;
+  private serietype=PublificationType.TVSHOW;
 
   constructor(private themoviedbService: ThemoviedbService) { }
 
   ngOnInit() {
     localStorage.setItem("tracked", JSON.stringify(
-      [{PDate:"19561",Name:"aedfed",Type:"movie",Genre:"Action, Comedy"},
-        {PDate:"5219",Name:"sdfrfvs",Type:"movie",Genre:"Drama, Comedy"}]
-    ));
-    this.movies=JSON.parse(localStorage.getItem("tracked"));
-  }
-private Onclick =(PDate,Name)=>{
-  let Remove = JSON.parse(localStorage.getItem("tracked"));
-  for(var i = Remove.length-1; i>=0; i--){
-    console.log(PDate,Name);
-    if (Remove[i].PDate === PDate && Remove[i].Name === Name) Remove.splice(i, 1);
-    console.log(Remove);
-  }
-  localStorage.setItem("tracked" ,JSON.stringify(Remove));
+      [
 
-}
+      ]
+    ))
+
+   var movies=JSON.parse(localStorage.getItem("tracked"))
+     .sort(function (a:Publification, b:Publification) {
+     if (a.release_date<b.release_date) {
+       return -1;
+     }
+     if (a.release_date>b.release_date) {
+       return 1;
+     }
+     // a must be equal to b
+     return 0;
+   });
+    this.movies=movies;
+    this.originalmovies=movies;
+  }
+
+  private Onclick =(PDate,Name)=>{
+    let Remove:Array<Publification> = JSON.parse(localStorage.getItem("tracked"));
+    for(var i = Remove.length-1; i>=0; i--){
+      console.log(PDate,Name);
+      if (Remove[i].release_date === PDate && Remove[i].name === Name) Remove.splice(i, 1);
+      console.log(Remove);
+    }
+    localStorage.setItem("tracked" ,JSON.stringify(Remove));
+
+  }
+
+  private checkboxChange = (type, check) =>{
+    if (check){
+      this.diplayedtypes.push(type)
+    }else {
+      this.diplayedtypes=this.diplayedtypes.filter(function (filttype) {
+        return filttype!=type
+      })
+    }
+    this.movies = this.originalmovies.filter((movie) => {
+      if (movie.type in this.diplayedtypes) {
+        return true;
+      }
+      return false;
+    })
+
+  }
 }
